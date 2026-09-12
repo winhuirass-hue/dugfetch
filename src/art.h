27acca9bb6e591bb6d7ed349e5295426d604662b
winhuirass-hue <winhuirass@gmail.com>
2026-09-12 17:33:33 +0300

    Refactor SHORK_COMP and SHORK_NORM art arrays

--- src/art.h
+++ src/art.h
@@ -18,28 +18,26 @@
 #define SHORK_COMP_WIDTH    16
 #define SHORK_COMP_HEIGHT   9
 const char SHORK_COMP[SHORK_COMP_HEIGHT][SHORK_COMP_WIDTH+1] = {
-    "                ",
-    "^`.             ",
-    "\\  \\            ",
-    "/   `~--__      ",
-    "          `~-_  ",
-    "      \\\\\\ __o,) ",
-    "~;   ,~~-`      ",
-    "'._.'           "
+    "   |\|\                ",
+    "  ..   \     .        ";
+    "o--    \\  / @)           ",
+    "v__//\\\__/ @           ",
+    "  {        }               "
+    "    { } /{ }      ",
+    "    <_|  <_|          "
+
 };
 
 #define SHORK_NORM_WIDTH    19
 #define SHORK_NORM_HEIGHT   10
 const char SHORK_NORM[SHORK_NORM_HEIGHT][SHORK_NORM_WIDTH+1] = {
-    "                   ",
-    "^`.                ",
-    "\\  \\               ",
-    "/   `~~-__         ",
-    "          `~~-_    ",
-    "        \\\\\\  o `.  ",
-    "  ,    ,    __,,,) ",
-    "~;   ,-~~--`       ",
-    "'._.'              "
+    "   |\|\       ",
+    " ..    \       .",
+    "o--     \\    / @) ",
+    "v__///\\\\__/ @ ",
+    "   {           }",
+    "    {  } \\\{  }",
+    "    <_|      <_|",
 };
 
 #endif
